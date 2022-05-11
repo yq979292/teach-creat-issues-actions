415fd477082de14b33ef85875ec5e874150d3d9e
yq979292 <319281998@qq.com>
2022-05-11 23:39:56 +0800

    test action 404 2

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 // // octokit 
 // // 客户端请github发送所有请求，可以实现对github所有操作：例如
 // // 获取用户信息，自动发布actions 发布 issure 等...
-// const {Octokit} =require('octokit');
+const {Octokit} =require('octokit');
 
 const core = require('@actions/core')
 // console.log(core.getInput('token'));
 
 // TODO auth
 let token = core.getInput('token');
-console.log('----------node run-------------');
 const octokit = new Octokit({
     auth: token,
-    // 如果 personal access token 没有创建；或者创建删除了 401 没权限
-    // access token  铭文传递github 会被自动三处
+    // access token  铭文传递github 会自动删除 
 });
 
 // octokit api create issure
@@ -21,7 +19,9 @@ const octokit = new Octokit({
 let res = octokit.rest.issues.create({
     owner: "yq979292", // github的用户名,路径
     repo: "teach-creat-issues-actions", // github仓库名；路径 
+
     // ---->以上这两个没写对会 报错 404
+
     title: "自定义issure标题, first issure", // 标题、
     body:'文章主体内容。哈哈哈sucessl'
 });
