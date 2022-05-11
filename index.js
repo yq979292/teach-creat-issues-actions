15a852f5350dccc5a240661498b31170b3219c01
yq979292 <319281998@qq.com>
2022-05-12 00:34:11 +0800

    更参考链接,测试没有权限401

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const {Octokit} =require('octokit');
 const core = require('@actions/core')
 
 // TODO auth
-let token = core.getInput('token');
+// let token = core.getInput('token');
 const octokit = new Octokit({
-    auth: token,
+    // auth: token,
+    auth:``,
     // access token  铭文传递github 会自动删除 
 });
 
